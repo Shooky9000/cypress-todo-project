2055fc8d95b7b545cea825d35a2f7e83bd65bb3c
Shooky9000 <surlan.surlan94@gmail.com>
2024-07-19 14:06:17 +0200

    fix github actions

--- cypress/support/e2e.ts
+++ cypress/support/e2e.ts
@@ -22,7 +22,7 @@ import addContext from 'mochawesome/addContext'
 Cypress.on("test:after:run", (test, runnable) => {
     
     let videoName = Cypress.spec.name
-    videoName = videoName.replace('/.ts.*', '.ts')
+    videoName = videoName.replace('/.cy.ts.*', '.ts')
     const videoUrl = 'videos/' + videoName + '.mp4'
 
     addContext({ test }, videoUrl)
